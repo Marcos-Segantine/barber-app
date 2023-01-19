35fc9bf1a328ef8b8e151b5592ebb03de4feee0f
Marcos Segantine <msegantine8@gmail.com>
2023-01-19 09:57:53 -0300

    Fixing bugs Co-authored-by: Victor Sabato Rocha <vsr@tuta.io>

--- src/screens/UserScreens/info-user-screens/Main.jsx
+++ src/screens/UserScreens/info-user-screens/Main.jsx
@@ -1,99 +1,95 @@
-import { View, StyleSheet, Pressable, Text } from 'react-native'
+import {View, StyleSheet, Pressable, Text} from 'react-native';
 
-import { useContext } from 'react'
+import {useContext} from 'react';
 
-import { Title } from '../../../components/Title'
+import {Title} from '../../../components/Title';
 
-import auth from '@react-native-firebase/auth'
+import auth from '@react-native-firebase/auth';
 
-import AsyncStorage from "@react-native-async-storage/async-storage"
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import { UserContext } from '../../../context/UserContext'
-import { useNavigation } from '@react-navigation/native'
+import {UserContext} from '../../../context/UserContext';
+import {useNavigation} from '@react-navigation/native';
 
 export const Main = () => {
-    const { userData, setUserData } = useContext(UserContext);
+  const {userData, setUserData} = useContext(UserContext);
 
-    const navigation = useNavigation()
+  const navigation = useNavigation();
 
-    const handleLogOut = async() => {
-        if(userData) 
-        {
-            const keys = ['@barber_app__email', '@barber_app__password']
-            await AsyncStorage.multiRemove(keys)
-                
-            auth().signOut()
-                .then(() => {
-                    setUserData(null)
-                    navigation.navigate("InitialScreen")
-                })
-        }
-    };
+  const handleLogOut = async () => {
+    if (userData) {
+      const keys = ['@barber_app__email', '@barber_app__password'];
+      await AsyncStorage.multiRemove(keys);
 
-    return(
-        <View style={style.container}>
-            <Title title={`Olá ${userData?.name}`} />
+      auth()
+        .signOut()
+        .then(() => {
+          setUserData(null);
+          navigation.navigate('InitialScreen');
+        });
+    }
+  };
 
-            <View style={style.contentLinks}>
-                <Pressable style={style.link}>
-                    <Text style={style.text} onPress={() => navigation.navigate("YourSchedules")}>
-                        Seus agendamentos
-                    </Text>
-                </Pressable>
+  return (
+    <View style={style.container}>
+      <Title title={`Olá ${userData?.name}`} />
 
-                <Pressable style={style.link} onPress={() => navigation.navigate("YourInformation")}>
-                    <Text style={style.text}>
-                        Suas informações
-                    </Text>
-                </Pressable>
+      <View style={style.contentLinks}>
+        <Pressable style={style.link}>
+          <Text
+            style={style.text}
+            onPress={() => navigation.navigate('YourSchedules')}>
+            Seus agendamentos
+          </Text>
+        </Pressable>
 
-                <Pressable style={style.link}>
-                    <Text style={style.text}>
-                        Redefinir senha
-                    </Text>
-                </Pressable>
+        <Pressable
+          style={style.link}
+          onPress={() => navigation.navigate('YourInformation')}>
+          <Text style={style.text}>Suas informações</Text>
+        </Pressable>
 
-                <Pressable style={style.link}>
-                    <Text style={style.text}>
-                        Enviar um feedback
-                    </Text>
-                </Pressable>
+        <Pressable style={style.link}>
+          <Text style={style.text}>Redefinir senha</Text>
+        </Pressable>
 
-                <Pressable style={style.link} onPress={handleLogOut}>
-                    <Text style={style.text}>
-                        Sair
-                    </Text>
-                </Pressable>
-            </View>
-        </View>
-    )
-}
+        <Pressable style={style.link}>
+          <Text style={style.text}>Enviar um feedback</Text>
+        </Pressable>
+
+        <Pressable style={style.link} onPress={handleLogOut}>
+          <Text style={style.text}>Sair</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
 
 const style = StyleSheet.create({
-    container: {
-        backgroundColor: "#1E1E1E",
-        flex: 1,
-        alignItems: 'center',
-    },
+  container: {
+    backgroundColor: '#1E1E1E',
+    flex: 1,
+    alignItems: 'center',
+  },
 
-    contentLinks: {
-        width: "100%",
-        alignItems: "center",
-        marginTop: 50,
-    },
+  contentLinks: {
+    width: '100%',
+    alignItems: 'center',
+    marginTop: 50,
+  },
 
-    link: {
-        width: "80%",
-        paddingVertical: 17,
-        borderWidth: 3,
-        borderColor: "#E95401",
-        marginVertical: 5,
-        borderRadius: 20,
-    },
+  link: {
+    width: '80%',
+    paddingVertical: 17,
+    borderWidth: 3,
+    borderColor: '#E95401',
+    marginVertical: 5,
+    borderRadius: 20,
+  },
 
-    text: {
-        color: "#FFFFFF",
-        fontWeight: '700',
-        textAlign: "center"
-    }
-})
+  text: {
+    color: '#FFFFFF',
+    fontWeight: '700',
+    textAlign: 'center',
+  },
+});
--- src/screens/UserScreens/info-user-screens/YourSchedules.jsx
+++ src/screens/UserScreens/info-user-screens/YourSchedules.jsx
@@ -1,86 +1,85 @@
-import { Text, View, StyleSheet, Pressable } from "react-native";
+import {Text, View, StyleSheet, Pressable} from 'react-native';
 
-import { Title } from "../../../components/Title";
-import { useContext, useEffect, useState } from "react";
+import {Title} from '../../../components/Title';
+import {useContext, useEffect, useState} from 'react';
 
-import { UserContext } from "../../../context/UserContext";
-import { useIsFocused, useNavigation } from "@react-navigation/native";
+import {UserContext} from '../../../context/UserContext';
+import {useIsFocused, useNavigation} from '@react-navigation/native';
 
 import firestore from '@react-native-firebase/firestore';
 
 export const YourSchedules = () => {
-    const [ schedules, setShedules ] = useState()
-    
-    const { userData } = useContext(UserContext)
+  const [schedules, setShedules] = useState(null);
 
-    const navigation = useNavigation()
+  const {userData} = useContext(UserContext);
 
-    const isFocused = useIsFocused()
+  const navigation = useNavigation();
 
-    useEffect(() => {
+  const isFocused = useIsFocused();
 
-        console.log("FOCUDED", "USER -> ", userData);
+  useEffect(() => {
+    firestore()
+      .collection('schedules_by_user')
+      .doc(userData.uid)
+      .get()
+      .then(res => {
+        setShedules(res._data.schedules);
+      });
+  }, [isFocused]);
 
-        firestore()
-            .collection("schedules_by_user")
-            .doc(userData.uid)
-            .get()
-            .then(({ _data }) => {
-                setShedules(_data.schedules)
-            })
+  return (
+    <View style={style.container}>
+      <Title title={'Seus agendamentos'} />
 
-    }, [ isFocused ])
-
-
-    return(
-        <View style={style.container}>
-            <Title title={"Seus agendamentos"} />
-
-            <View style={style.content}>
-                {
-                    schedules ?
-
-                    schedules.map((item, index) => {
-                        return(
-                            <Pressable style={style.schedulesDay} key={index} onPress={() => navigation.navigate("ScheduleDetail", {item})}>
-                                <Text style={style.text}>Dia: {item.day.split('-').reverse().join('/ ')}</Text>
-                                <Text style={style.text}>Horario: {item.shedule}</Text>
-                            </Pressable>            
-                        )
-                    }) :
-                    <Text style={style.text}>Sem Horarios marcados</Text>
-                }
-            </View>
-        </View>
-    )
-}
+      <View style={style.content}>
+        {schedules ? (
+          schedules.map((item, index) => {
+            return (
+              <Pressable
+                style={style.schedulesDay}
+                key={index}
+                onPress={() => navigation.navigate('ScheduleDetail', {item})}>
+                <Text style={style.text}>
+                  Dia: {item?.day?.split('-').reverse().join('/ ')}
+                </Text>
+                <Text style={style.text}>Horario: {item.shedule}</Text>
+              </Pressable>
+            );
+          })
+        ) : (
+          <Text style={style.text}>Sem Horarios marcados</Text>
+        )}
+      </View>
+    </View>
+  );
+};
 
 const style = StyleSheet.create({
-    container: {
-        backgroundColor: "#1E1E1E",
-        flex: 1,
-        alignItems: 'center',
-    },
+  container: {
+    backgroundColor: '#1E1E1E',
+    flex: 1,
+    alignItems: 'center',
+  },
 
-    content: {
-        width: "100%",
-        alignItems: "center",
-        marginTop: 70,
-    },
+  content: {
+    width: '100%',
+    alignItems: 'center',
+    marginTop: 70,
+  },
 
-    schedulesDay: {
-        borderWidth: 3,
-        borderRadius: 20,
-        borderColor: "#E95401",
-        width: "80%",
-        paddingVertical: 15,
-        alignItems: "center",
-        marginTop: 20
-    },
+  schedulesDay: {
+    borderWidth: 3,
+    borderRadius: 20,
+    borderColor: '#E95401',
+    width: '80%',
+    paddingVertical: 15,
+    alignItems: 'center',
+    marginTop: 20,
+  },
 
-    text: {
-        color: "#FFFFFF",
-        fontWeight: '700',
-        fontSize: 17
-    }
-})
+  text: {
+    color: '#FFFFFF',
+    fontWeight: '700',
+    fontSize: 17,
+  },
+});
