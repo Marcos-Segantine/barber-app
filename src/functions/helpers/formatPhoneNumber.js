0cbeb7349bfef3fc0f274405e45fc7c0b16a9754
Marcos Segantine <msegantine8@gmail.com>
2023-02-24 19:55:55 -0300

    Improving phone formated function

--- src/functions/helpers/formatPhoneNumber.js
+++ src/functions/helpers/formatPhoneNumber.js
@@ -1,11 +1,22 @@
 export const formatPhoneNumber = text => {
-  let cleaned = ('' + text).replace(/\D/g, '');
-  let match = cleaned.match(/^(\d{2})(\d{5})(\d{4})$/);
-  if (match) {
-    return '(' + match[1] + ') ' + match[2] + '-' + match[3];
-  } else {
-    return cleaned
-      .substring(0, 11)
-      .replace(/^(\d{2})(\d{0,5})(\d{0,4}).*/, '($1) $2-$3');
+  const cleaned = text.toString().replace(/\D/g, '');
+  const groups = {
+    0: cleaned.slice(0, 2),
+    1: cleaned.slice(2, 7),
+    2: cleaned.slice(7, 11),
+  };
+
+  if (text.length === 1) {
+    return text;
   }
+
+  if (cleaned.length === 2) {
+    return `(${groups[0]})`;
+  }
+
+  if (cleaned.length <= 7) {
+    return `(${groups[0]}) ${groups[1]}`;
+  }
+
+  return `(${groups[0]}) ${groups[1]}-${groups[2]}`;
 };
