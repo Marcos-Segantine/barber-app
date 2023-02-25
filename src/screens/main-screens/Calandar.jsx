b32f13030852f1db351b67acc487573ab5420133
Marcos Segantine <msegantine8@gmail.com>
2023-02-25 16:31:36 -0300

    Improving calandar UI

--- src/screens/main-screens/Calandar.jsx
+++ src/screens/main-screens/Calandar.jsx
@@ -67,7 +67,8 @@ export const Calandar = ({navigation}) => {
       .collection('denied_days')
       .get()
       .then(async res => {
-        setDeniedDays(res._docs[month - 1] ? res._docs[month - 1]._data : {});
+        const deniedDays = res._docs;
+        deniedDays.map(data => (data._data ? setDeniedDays(data._data) : {}));
       });
   }, []);
 
@@ -80,56 +81,61 @@ export const Calandar = ({navigation}) => {
   const handleLeftArrow = () => setMonth(month === 1 ? 12 : month - 1);
   const handleRightArrow = () => setMonth(month === 12 ? 1 : month + 1);
 
+  if (!deniedDays) {
+    return (
+      <View style={style.container}>
+        <LoadingAnimation />
+      </View>
+    );
+  }
+
   return (
     <View style={style.container}>
       <Title title="Selecione um data" />
 
-      {deniedDays ? (
-        <Calendar
-          context={{date: ''}}
-          onPressArrowLeft={subtractMonth => {
-            handleLeftArrow();
-            subtractMonth();
-          }}
-          onPressArrowRight={addMonth => {
-            handleRightArrow();
-            addMonth();
-          }}
-          minDate={String(new Date())}
-          markedDates={{
-            ...deniedDays,
-            [shedulesUser.day]: {
-              selected: true,
-              marked: true,
-              selectedColor: 'white',
-            },
-          }}
-          // deniedDays
-          onDayPress={day =>
-            setShedulesUser({...shedulesUser, day: day.dateString})
-          }
-          disableArrowLeft={arrawLeftAvaible}
-          style={{
-            width: 350,
-            marginTop: 40,
-            padding: 5,
-            borderRadius: 20,
-          }}
-          theme={{
-            calendarBackground: '#E95401',
-            dayTextColor: '#FFFFFF',
-            selectedDayTextColor: '#E95401',
-            selectedDayBackgroundColor: '#FFFFFF',
-            textDisabledColor: '#FFFFFF40',
-            textSectionTitleColor: '#FFFFFF',
-            arrowColor: '#FFFFFF',
-            monthTextColor: '#FFFFFF',
-            textDayHeaderFontWeight: '700',
-          }}
-        />
-      ) : (
-        <LoadingAnimation />
-      )}
+      <Calendar
+        context={{date: ''}}
+        onPressArrowLeft={subtractMonth => {
+          handleLeftArrow();
+          subtractMonth();
+        }}
+        onPressArrowRight={addMonth => {
+          handleRightArrow();
+          addMonth();
+        }}
+        minDate={String(new Date())}
+        markedDates={{
+          ...deniedDays,
+          [shedulesUser.day]: {
+            selected: true,
+            marked: true,
+            selectedColor: 'white',
+          },
+        }}
+        // deniedDays
+        onDayPress={day =>
+          setShedulesUser({...shedulesUser, day: day.dateString})
+        }
+        disableArrowLeft={arrawLeftAvaible}
+        style={{
+          width: 350,
+          marginTop: 40,
+          padding: 5,
+          borderRadius: 20,
+        }}
+        theme={{
+          calendarBackground: '#E95401',
+          dayTextColor: '#FFFFFF',
+          selectedDayTextColor: '#E95401',
+          selectedDayBackgroundColor: '#FFFFFF',
+          textDisabledColor: '#FFFFFF40',
+          textSectionTitleColor: '#FFFFFF',
+          arrowColor: '#FFFFFF',
+          monthTextColor: '#FFFFFF',
+          textDayHeaderFontWeight: '700',
+        }}
+      />
+
       <Button
         text="Comfirmar"
         action={handleButton}
@@ -143,7 +149,7 @@ const style = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    backgroundColor: '#1E1E1E',
     justifyContent: 'space-between',
+    backgroundColor: '#1E1E1E',
   },
 });
