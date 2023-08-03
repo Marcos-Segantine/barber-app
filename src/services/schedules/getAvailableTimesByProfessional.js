f2aa915eb92cbc9b0a5b2df068193f4d13d226e5
Marcos Segantine <msegantine8@gmail.com>
2023-08-03 20:54:58 -0300

    Improving security from newest schedules 1/2

--- src/services/schedules/getAvailableTimesByProfessional.js
+++ src/services/schedules/getAvailableTimesByProfessional.js
@@ -1,6 +1,8 @@
 import firestore from '@react-native-firebase/firestore';
 
 import { getDay, getMonth, getProfessional, getYear } from '../../utils/dateHelper';
+import { getCurrentHour } from '../../utils/getCurrentHour';
+import { getWeekDay } from '../../utils/getWeekDay';
 
 export const getAvailableTimesByProfessional = async (
     scheduleInfo,
@@ -8,28 +10,42 @@ export const getAvailableTimesByProfessional = async (
     setSomethingWrong
 ) => {
     try {
-        
+
         const year = getYear(scheduleInfo);
         const month = getMonth(scheduleInfo);
         const day = getDay(scheduleInfo);
         const professional = getProfessional(scheduleInfo)
 
-        const currentDay = new Date(scheduleInfo.day).getDay() + 1
-        const weekday = currentDay <= 5 ? "weekday" : currentDay === 6 ? "saturday" : "sunday"
+        const weekday = getWeekDay(scheduleInfo.day)
 
         const unavailableTimesRef = firestore().collection("unavailable_times").doc(`${month}_${year}`)
         const workingHoursRef = firestore().collection('working_hours').doc(professionalUid)
 
-        const workingHoursData = (await workingHoursRef.get({ source: "server" })).data()
+        const workingHoursData = (await workingHoursRef.get({ source: "server" })).data()[weekday]
         const unavailableTimesData = (await unavailableTimesRef.get({ source: "server" })).data()
+
+        const currentDay = new Date().getDate();
+        const currentMonth = new Date().getMonth() + 1;
+        const currentHour = getCurrentHour();
+
+        const currentDate = Number(day) === Number(currentDay) && Number(currentMonth) === Number(month)
 
         if (workingHoursData === undefined) return undefined
 
-        if (!unavailableTimesData) return workingHoursData[weekday]
-        else if (!unavailableTimesData[day]) return workingHoursData[weekday]
-        else if (!unavailableTimesData[day][professional]) return workingHoursData[weekday]
+        if (!unavailableTimesData) return workingHoursData
+        else if (unavailableTimesData[day] && currentDate)
+            return workingHoursData.filter(schedule => Number(schedule.split(":")[0]) > Number(currentHour) && !unavailableTimesData[day][professional].includes(schedule))
+        else if (!unavailableTimesData[day] && currentDate)
+            return workingHoursData.filter(schedule => Number(schedule.split(":")[0]) > Number(currentHour))
+        else if (unavailableTimesData[day]?.[professional] && currentDate)
+            return workingHoursData.filter(schedule => Number(schedule.split(":")[0]) > Number(currentHour) && !unavailableTimesData[day][professional].includes(schedule))
+        else if (!unavailableTimesData[day]?.[professional] && currentDate)
+            return workingHoursData.filter(schedule => Number(schedule.split(":")[0]) > Number(currentHour))
 
-        const dataTemp = workingHoursData[weekday].filter(time => {
+        else if (!unavailableTimesData[day]) return workingHoursData
+        else if (!unavailableTimesData[day][professional]) return workingHoursData
+
+        const dataTemp = workingHoursData.filter(time => {
             return !unavailableTimesData[day][professional].includes(time)
         })
 
--- src/utils/getWeekDay.js
+++ src/utils/getWeekDay.js
@@ -1,7 +1,7 @@
 export const getWeekDay = (dateString) => {
     const [year, month, day] = dateString.split('-').map(Number);
     const date = new Date(year, month - 1, day);
-    const dayOfWeek = date.getDay(); // 0 (Sunday) to 6 (Saturday)
+    const dayOfWeek = date.getDay();
 
     if(dayOfWeek === 0) return "sunday"
     else if(dayOfWeek === 6) return "saturday"
