82883aa4cc8cadac46096f021e8ad9d37a1515c8
Marcos Segantine <msegantine8@gmail.com>
2023-09-11 14:31:54 -0300

    Improving util that get first and last name from user

--- src/utils/getNameLastName.js
+++ src/utils/getNameLastName.js
@@ -1,3 +1,8 @@
 export const getNameLastName = (name) => {
-    return name.split(" ").splice(0, 2).join(" ")
+    name = name.split(" ")
+
+    const firstName = name[0]
+const lastName = name[1].length < 5 ? name[1] + " " + name[2] : name[1]
+
+    return firstName + " " + lastName
 }
