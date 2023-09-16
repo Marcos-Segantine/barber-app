ae1e3f3e8607192dc5ea0652e09000575e470782
Marcos Segantine <msegantine8@gmail.com>
2023-09-16 14:57:09 -0300

    Fixing function that verify if user can cancel your schedule

--- src/validation/verifyIfUserCanCancelSchedule.js
+++ src/validation/verifyIfUserCanCancelSchedule.js
@@ -2,7 +2,7 @@ export const verifyIfUserCanCancelSchedule = (minimalHoursToCancelSchedule, Sche
     const [year, month, day] = ScheduleDay.split('-').map(Number);
     const [hours, minutes] = scheduleHour.split(':').map(Number);
 
-    const userScheduledDateTime = new Date(year, month - 1, day, hours, minutes);
+    const userScheduledDateTime = new Date(year, month, day, hours, minutes);
 
     const currentDateTime = new Date();
 
@@ -12,9 +12,12 @@ export const verifyIfUserCanCancelSchedule = (minimalHoursToCancelSchedule, Sche
 
     if (hoursDifference >= minimalHoursToCancelSchedule) {
         if (timeDifference > 0) {
+            return true;
         } else {
+            return false;
         }
     } else {
+        return false;
     }
 
 }
